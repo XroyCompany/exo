bd0d9b339b6be79c266297f94d9ae544811d1561
Eldarik25 <Fot1380123@mail.ru>
2024-04-02 17:33:45 +0300

    Initial Commit

---
+++ Exostyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exostyle
+//
+//  Created by Ибрагимов Эльдар on 02.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
